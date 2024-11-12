169a00755b782896490fc7409e48aa8f0de11ee3
Sergey Nikolenko <git.user.snikolenko.s.y@gmail.com>
2024-11-12 21:17:40 +0300

    изменили печать doSomething

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -10,8 +10,6 @@ public class MainApplication {
     }
 
     public static void doSomething(){
-        System.out.println("0");
-        System.out.println("0");
-        System.out.println("0");
+        System.out.println("Hello World!");
     }
 }
