649af2830de9158eb50347cd753f74b2590a8d27
Sergey Nikolenko <git.user.snikolenko.s.y@gmail.com>
2024-11-12 21:03:20 +0300

    изменили логику main doSomething

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -2,13 +2,14 @@ package com.stepup;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("------");
         System.out.println("Hello World!");
         System.out.println("111111");
     }
 
     public static void doSomething(){
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
+        System.out.println("0");
+        System.out.println("0");
+        System.out.println("0");
     }
 }
